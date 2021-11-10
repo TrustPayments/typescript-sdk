ed474c4ba4d2876aadf0bbfbe86a637a1d126643
Victor Ndambakuwa <victor.ndambakuwa@wallee.com>
2021-11-10 14:36:08 +0100

    Release 3.0.5

--- src/serializers/ObjectSerializer.ts
+++ src/serializers/ObjectSerializer.ts
@@ -250,6 +250,8 @@ import { TransactionInvoiceComment } from "../models/TransactionInvoiceComment";
 import { TransactionInvoiceReplacement } from "../models/TransactionInvoiceReplacement";
 import { TransactionInvoiceState } from "../models/TransactionInvoiceState";
 import { TransactionLineItemUpdateRequest } from "../models/TransactionLineItemUpdateRequest";
+import { TransactionLineItemVersionCreate } from "../models/TransactionLineItemVersionCreate";
+import { TransactionLineItemVersionState } from "../models/TransactionLineItemVersionState";
 import { TransactionState } from "../models/TransactionState";
 import { TransactionUserInterfaceType } from "../models/TransactionUserInterfaceType";
 import { TransactionVoidMode } from "../models/TransactionVoidMode";
@@ -407,6 +409,7 @@ class ObjectSerializer {
         "TransactionEnvironmentSelectionStrategy": TransactionEnvironmentSelectionStrategy,
         "TransactionGroupState": TransactionGroupState,
         "TransactionInvoiceState": TransactionInvoiceState,
+        "TransactionLineItemVersionState": TransactionLineItemVersionState,
         "TransactionState": TransactionState,
         "TransactionUserInterfaceType": TransactionUserInterfaceType,
         "TransactionVoidMode": TransactionVoidMode,
@@ -594,6 +597,7 @@ class ObjectSerializer {
                 "TransactionInvoiceComment": TransactionInvoiceComment,
                 "TransactionInvoiceReplacement": TransactionInvoiceReplacement,
                 "TransactionLineItemUpdateRequest": TransactionLineItemUpdateRequest,
+                "TransactionLineItemVersionCreate": TransactionLineItemVersionCreate,
                 "TwoFactorAuthenticationType": TwoFactorAuthenticationType,
                 "User": User,
                 "UserAccountRole": UserAccountRole,
